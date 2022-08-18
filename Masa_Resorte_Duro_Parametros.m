b5ab5aaab590b6e86c8bee2e5d6305932f81d004
joselpj <joselu.pjaramillo@gmail.com>
2022-08-18 18:17:13 -0500

    archivos adicionados al repostiorio

---
+++ Masa_Resorte_Duro_Parametros.m
@@ -0,0 +1,9 @@
+%Parametros Masa-Resorte_Duro
+%The data was generated for a=0,a=1,a=5,a=10,a=15 19/sept/2019
+k=0.1;
+m=1;
+F=5;
+a=15;
+B=0.5;
+Ci1=0;
+Ci2=0;
---
+++ Pendulo_SNL_Parametros.m
@@ -0,0 +1,7 @@
+%Parametros Pendulo
+g=9.8;
+k=0.5;
+L=0.5;
+m=1;
+Ci1=pi/4;
+Ci2=0;
